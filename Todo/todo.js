102a803469e8d6cf68616261fa307bce3b0616b9
aueaoangkung <sroiaudom@gmail.com>
2021-11-22 01:45:37 +0700

    145

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,62 +1,16 @@
-const inputBox = document.querySelector(".inputField input");
-const addBtn = document.querySelector(".inputField button");
-const todoList = document.querySelector(".todoList");
-const deleteAllBtn = document.querySelector(".footer button");
-
-inputBox.onkeyup = ()=>{
-  let userEnteredValue = inputBox.value; 
-  if(userEnteredValue.trim() != 0){ 
-    addBtn.classList.add("active"); 
-  }else{
-    addBtn.classList.remove("active"); 
-  }
-}
-showTasks(); 
-addBtn.onclick = ()=>{ 
-  let userEnteredValue = inputBox.value; 
-  let getLocalStorageData = localStorage.getItem("New Todo"); 
-  if(getLocalStorageData == null){ 
-    listArray = []; 
-  }else{
-    listArray = JSON.parse(getLocalStorageData);  
-  }
-  listArray.push(userEnteredValue); 
-  localStorage.setItem("New Todo", JSON.stringify(listArray)); 
-  showTasks(); 
-  addBtn.classList.remove("active"); 
-}
-function showTasks(){
-  let getLocalStorageData = localStorage.getItem("New Todo");
-  if(getLocalStorageData == null){
-    listArray = [];
-  }else{
-    listArray = JSON.parse(getLocalStorageData); 
-  }
-  const pendingTasksNumb = document.querySelector(".pendingTasks");
-  pendingTasksNumb.textContent = listArray.length; 
-  if(listArray.length > 0){ 
-    deleteAllBtn.classList.add("active"); 
-  }else{
-    deleteAllBtn.classList.remove("active"); 
-  }
-  let newLiTag = "";
-  listArray.forEach((element, index) => {
-    newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
-  });
-  todoList.innerHTML = newLiTag; 
-  inputBox.value = ""; 
-}
-
-function deleteTask(index){
-  let getLocalStorageData = localStorage.getItem("New Todo");
-  listArray = JSON.parse(getLocalStorageData);
-  listArray.splice(index, 1); 
-  localStorage.setItem("New Todo", JSON.stringify(listArray));
-  showTasks(); 
-}
-
-deleteAllBtn.onclick = ()=>{
-  listArray = []; 
-  localStorage.setItem("New Todo", JSON.stringify(listArray)); 
-  showTasks(); 
-}
+$('input').keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$('ul').append('<li>' + todoText + '<span><i class="fa fa-trash"</i></span>');
+	}
+});
+$('ul').on('click', "span" , function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+$('ul').on('click', 'li', function(){
+	$(this).toggleClass('done');
+});
